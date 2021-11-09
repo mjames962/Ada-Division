d16d96b3ff83c2d8bd0e30505d6eccd6090cbf65
mjames962 <75989077+mjames962@users.noreply.github.com>
2021-11-09 10:39:33 +0000

    Dependency fix and verification conditions

--- src/division.adb
+++ src/division.adb
@@ -5,33 +5,39 @@ use AS_Io_Wrapper, SPARK.Text_IO;
 
 package body Division is
 
-   procedure div (dividend : in Integer; divisor : in Integer) is
+   procedure div
+     (dividend : in     Integer; divisor : in Integer; remainder : out Integer;
+      quotient :    out Integer)
+   is
 
-      remainder : Integer;
-      quotient  : Integer;
+   -- Dividend = (quotient * divisor) + remainder
+   -- quotient = (dividend - remainder)/divisor
 
    begin
+
       remainder := dividend;
       quotient  := 0;
 
       loop
+
          exit when divisor > remainder;
+
+         pragma Loop_Invariant
+           (dividend = (quotient * divisor) + remainder and
+            divisor <= remainder);
 
          remainder := remainder - divisor;
          quotient  := quotient + 1;
 
       end loop;
 
-      AS_Put_Line ("Quotient:");
-      AS_Put_Line (quotient);
-      AS_Put_Line ("Remainder:");
-      AS_Put_Line (remainder);
-
    end div;
 
    procedure run is
-      dividend : Integer;
-      divisor  : Integer;
+      dividend  : Integer;
+      divisor   : Integer;
+      remainder : Integer;
+      quotient  : Integer;
 
    begin
 
@@ -40,16 +46,30 @@ package body Division is
       AS_Init_Standard_Input;
       AS_Put_Line ("Calculate quotient and remainder");
 
-      -- Print on screen "Type in some number"
-      AS_Put_Line ("Type in dividend");
+      loop
+         -- Print on screen "Type in some number"
+         AS_Put_Line ("Type in a positive dividend");
 -- ask for an  integer (in variable dividend) until an integer has been entered
-      --   if something else has been entered ask again
-      AS_Get (dividend, "Please type in an integer; please try again");
+         --   if something else has been entered ask again
+         AS_Get (dividend, "Please type in an integer; please try again");
+         exit when dividend >= 0 and dividend <= Integer'Last;
+      end loop;
 
-      AS_Put_Line ("Type in divisor");
-      AS_Get (divisor, "Please type in an integer; please try again");
+      loop
+         AS_Put_Line ("Type in a positive divisor");
+         AS_Get (divisor, "Please type in an integer; please try again");
+         exit when divisor >= 1 and divisor <= Integer'Last and
+           divisor <= dividend;
+      end loop;
 
-      Division.div (dividend => dividend, divisor => divisor);
+      Division.div
+        (dividend => dividend, divisor => divisor, remainder => remainder,
+         quotient => quotient);
+
+      AS_Put_Line ("Quotient:");
+      AS_Put_Line (quotient);
+      AS_Put_Line ("Remainder:");
+      AS_Put_Line (remainder);
 
       null;
    end run;
--- src/division.ads
+++ src/division.ads
@@ -5,9 +5,15 @@ use AS_Io_Wrapper, SPARK.Text_IO;
 
 package Division is
 
-   procedure div (dividend : in Integer; divisor : in Integer) with
-      Global  => (In_Out => Standard_Output),
-      Depends => (Standard_Output => (Standard_Output, dividend, divisor));
+   procedure div
+     (dividend : in     Integer; divisor : in Integer; remainder : out Integer;
+      quotient :    out Integer) with
+      Depends => (remainder => (dividend, divisor),
+       quotient => (divisor, dividend)),
+      Pre  => (dividend >= 0 and divisor >= 1 and dividend >= divisor),
+      Post =>
+      (remainder < divisor and
+       dividend = ((quotient * divisor) + remainder) and remainder >= 0);
 
    procedure run;
 
